ee52dba772fe9106b1772cb4473475cb5434e358
Diego <diego@diego.com>
2019-08-27 13:53:29 +0200

    Diego

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,23 +4,37 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('plainPassword', PasswordType::class, [
+        
+            ->add('email',EmailType::class,[
+            'label' => false,
+            
+            'attr' =>[
+                'placeholder'=>'Email',
+                'class'=>"form-control"
+            ]
+            ])
+            ->add('password', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => false,
+                'attr' =>[
+                    'placeholder'=>'Contraseña',
+                    'class'=>"form-control"
+                ],
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
