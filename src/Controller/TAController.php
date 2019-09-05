d8df55b3996e6551ffe8b80a673f177c5480ebd5
Maria <mariasolanasalfranca@gmail.com>
2019-09-05 10:14:34 +0200

    fin

--- src/Controller/TAController.php
+++ src/Controller/TAController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Grupo;
 use App\Repository\GrupoRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/", name="")
@@ -24,6 +25,21 @@ class TAController extends AbstractController
         return $this->render('ta/index.html.twig', [
             'grupos' => $grupos
         ]);
+    }
+
+    /**
+    * @Route("/{idGrupo}/{precio}", name="index2")
+    */
+    public function index2($idGrupo, $precio, EntityManagerInterface $em)
+    {
+        $dql = "select p from \App\Entity\Pack p join p.grupo g where p.precio < :precio and g.id = :grupo";
+        $query=$em->createQuery($dql);
+        $query->setParameter('precio', $precio);
+        $query->setParameter('grupo', $grupo);
+        
+        $packs=$query->execute();
+        
+        return $this->json($packs);
     }
 
     /**
