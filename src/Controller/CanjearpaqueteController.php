01795521c5749f7be244a3289fe6e7e12664cbab
Dani <breano33@gmail.com>
2019-09-06 10:06:14 +0200

    final dani

--- src/Controller/CanjearpaqueteController.php
+++ src/Controller/CanjearpaqueteController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\LineaPedidoRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CanjearpaqueteController extends AbstractController
 {
@@ -14,6 +16,27 @@ class CanjearpaqueteController extends AbstractController
     {
         return $this->render('canjearpaquete/index.html.twig', [
             'controller_name' => 'CanjearpaqueteController',
+        ]);
+    }
+    /**
+     * @Route("/verificar", name="verificar")
+     */
+    public function comprobar( Request $request,  LineaPedidoRepository $pedidoR)
+    {
+        
+
+        $pedido=$pedidoR->find($request->request->get('codigo'));
+
+        if($pedido){
+          
+           
+        }else{
+            
+        }
+
+        return $this->render('canjearpaquete/index.html.twig', [
+            'controller_name' => 'CanjearpaqueteController',
+            'pedido' => $pedido
         ]);
     }
 }
